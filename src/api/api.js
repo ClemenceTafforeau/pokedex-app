506a452bb48c17ee9605b87081b3415f63cfbd5f
Clemence <clem.tafforeau@gmail.com>
2025-06-13 16:52:48 +0200

    feat(api): Setting up the functions fetching data from API endpoints

---
+++ src/api/api.js
@@ -0,0 +1,36 @@
+const baseUrl = import.meta.env.VITE_API_URL;
+
+export function getPkmnImg(id) {
+    return `raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`;
+}
+
+export async function getAllPkmns() {
+    const url = `${baseUrl}pokemons`;
+
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+
+            return await response.json();
+        }
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+export async function getPkmn(id) {
+    const url = `${baseUrl}pokemons/${id}`;
+}
+
+export async function updatePkmn(id, content) {
+    const url = `${baseUrl}pokemons/${id}`;
+}
+
+export async function getAllReviews(id) {
+    const url = `${baseUrl}reviews/?pokemonId=${id}`;
+}
+
+export async function postReview(review) {
+    const url = `${baseUrl}reviews/`;
+    const body = JSON.stringify(review);
+}
